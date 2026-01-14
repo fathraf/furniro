45e86244478426d8187d9940d6047c00a6f4b6d5
Fathima Rafeeda <fathimarafeeda280@gmail.com>
2026-01-14 11:55:17 +0530

    Add JS logic for side cart toggle

--- script.js
+++ script.js
@@ -20,3 +20,22 @@ const unitPrice = 2500.000;
         priceEl.textContent = `Rs ${(unitPrice * quantity).toFixed(2)}`;
         decreaseBtn.disabled = quantity === 1;
       }
+
+      const addToCartBtn =document.getElementById("addToCartBtn");
+      const sideCart = document.getElementById("sideCart");
+      const closeCartBtn = document.querySelector(".close-cart");
+      const overlay = document.getElementById("cartOverlay")
+
+      addToCartBtn.addEventListener("click", () => {
+        sideCart.classList.add("cart-open");
+        overlay.classList.add("active");
+});
+      closeCartBtn.addEventListener("click", () =>{
+        sideCart.classList.remove("cart-open");
+        overlay.classList.remove("active")
+      })
+      // cart closes when clicking overlay
+      overlay.addEventListener("click", () => {
+        sideCart.classList.remove("cart-open")
+        overlay.classList.remove("active")
+      })
