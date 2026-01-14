84dc38664123d398295e0afb2ee64c868a3331a7
Fathima Rafeeda <fathimarafeeda280@gmail.com>
2026-01-14 11:14:50 +0530

    add js

---
+++ script.js
@@ -0,0 +1,22 @@
+const unitPrice = 2500.000;
+      let quantity = 1;
+
+      const quantityEl = document.querySelector('.quantity');
+      const priceEl = document.querySelector('.product-info__price');
+      const increaseBtn = document.getElementById('inc');
+      const decreaseBtn = document.getElementById('dec');
+
+      increaseBtn.addEventListener("click", function(){
+        quantity++;
+        updateUI();
+      })
+      decreaseBtn.addEventListener("click", function(){
+        quantity--;
+        updateUI()
+      })
+
+      function updateUI() {
+        quantityEl.textContent = quantity;
+        priceEl.textContent = `Rs ${(unitPrice * quantity).toFixed(2)}`;
+        decreaseBtn.disabled = quantity === 1;
+      }
